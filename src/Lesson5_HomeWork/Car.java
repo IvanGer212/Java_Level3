019a21c4dc8c9d403972e5188ba12f3c985ab649
IvanGer212 <Comradeivan88@gmail.com>
2021-05-02 19:36:24 +0300

    Change the program for all cars start race together

--- src/Lesson5_HomeWork/Car.java
+++ src/Lesson5_HomeWork/Car.java
@@ -1,10 +1,13 @@
 package Lesson5_HomeWork;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     static {
         CARS_COUNT = 0;
     }
+    private final CountDownLatch countDownLatch;
     private Race race;
     private int speed;
     private String name;
@@ -14,23 +17,35 @@ public class Car implements Runnable {
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed,CountDownLatch countDownLatch) {
         this.race = race;
+        this.countDownLatch = countDownLatch;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
     @Override
     public void run() {
+        boolean startRace;
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            countDownLatch.countDown();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        startRace = true;
+        if (startRace) {
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
+        }
         }
     }
 }
