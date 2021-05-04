04ea55e46c1742c1cf259463feb72a524ae03402
IvanGer212 <Comradeivan88@gmail.com>
2021-05-04 20:45:10 +0300

    Write test and program for first task.

--- src/Lesson5_HomeWork/MainClass.java
+++ src/Lesson5_HomeWork/MainClass.java
@@ -12,7 +12,7 @@ public class MainClass {
         //CyclicBarrier cyclicBarrier1 = new CyclicBarrier(2);
         StarterCyclicBarier starterCyclicBarier = new StarterCyclicBarier(cyclicBarrier);
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(2), new Road(40));
+        Race race = new Race(new Road(60), new Tunnel(CARS_COUNT/2), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10),countDownLatch, cyclicBarrier);
---
+++ src/Lesson6_HomeWork/Tests/Test1.java
@@ -0,0 +1,39 @@
+package Lesson6_HomeWork.Tests;
+
+import Lesson6_HomeWork.Main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.stream.Stream;
+
+public class Test1 {
+    @ParameterizedTest
+    @NullAndEmptySource
+    void SouldThrowIllegalArgumentExceptionWhenEmptyArrayPassed(int[] emptyArray){
+        Assertions.assertThrows(IllegalArgumentException.class, ()->Main.extractAfterLastFour(emptyArray));
+    }
+
+    @ParameterizedTest
+    @MethodSource("validArrayParameterProvider")
+    void ShouldThrowRuntimeExceptionWhenEmptyArrayPassed(int[] inputArray, int[] expectedArray){
+        Assertions.assertArrayEquals(expectedArray, Main.extractAfterLastFour(inputArray));
+    }
+
+    private static Stream<Arguments> validArrayParameterProvider(){
+        return Stream.of(
+                Arguments.of(new int[] {1, 2, 4, 4, 2, 3, 4, 1, 7}, new int[] {1,7}),
+                Arguments.of(new int[] {1, 2, 4, 4, 2, 3, 5, 1, 7}, new int[] {2, 3, 5, 1, 7}),
+                Arguments.of(new int[] {4, 1}, new int[] {1})
+                );
+    }
+
+    @Test
+    void ShouldThrowRuntimeExceptionWhenInArrayNoFourth(){
+        Assertions.assertThrows(RuntimeException.class, ()->Main.extractAfterLastFour(new int[] {2}));
+    }
+}
